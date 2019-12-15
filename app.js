a0a859f47889a2eb8eafd884244348e0b7235f4b
pawelkatny <pawel.katny@gmail.com>
2019-12-15 02:26:07 +0100

    15/12/2019 Adds few functions: - calculating budget data - adding new object to the data structure - displaying that object on the UI - clearing data (when clicking on menu icon "Clear data")

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ let dataController = (function () {
 
     let Income = function (id, desc, value) {
         this.id = id,
-        this.desc = desc,
-        this.value = value
-    }
+            this.desc = desc,
+            this.value = value
+    };
 
     let Expense = function (id, desc, value) {
         this.id = id,
-        this.desc = desc,
-        this.value = value
-    }
+            this.desc = desc,
+            this.value = value
+    };
 
     let budgetDATA = {
         month: null,
         year: null,
-        budget: 0,
+        balance: 0,
         items: {
             inc: [],
             exp: []
@@ -27,11 +27,68 @@ let dataController = (function () {
             inc: 0,
             exp: 0
         }
+    };
+
+    let calcTotal = function (type) {
+        let total = 0;
+        budgetDATA.items[type].forEach((ele) => {
+            total += Number(ele.value);
+        })
+
+        budgetDATA.total[type] = total;
     }
 
     return {
-        getData: function() {
-            return budgetDATA;
+        getData: function () {
+            return {
+                balance: budgetDATA.balance,
+                inc: budgetDATA.total['inc'],
+                exp: budgetDATA.total['exp']
+            };
+        },
+
+        addItem: function (type, object) {
+            let ID, newItem, budget;
+            budget = budgetDATA.items[type];
+
+            if (budget.length == -1) {
+                ID = 0;
+            } else {
+                ID = budget.length;
+            }
+
+            if (type === 'inc') {
+                newItem = new Income(ID, object.desc, object.value);
+            } else if (type === 'exp') {
+                newItem = new Expense(ID, object.desc, object.value);
+            }
+
+            budget.push(newItem);
+
+            return newItem;
+
+        },
+
+        calcBudget: function () {
+            calcTotal('inc');
+            calcTotal('exp');
+            budgetDATA.balance = budgetDATA.total['inc'] - budgetDATA.total['exp']
+        },
+
+        clearBudget: function () {
+            budgetDATA.balance = 0;
+            budgetDATA.items = {
+                inc: [],
+                exp: []
+            };
+            budgetDATA.total = {
+                inc: 0,
+                exp: 0
+            };
+        },
+
+        test: function () {
+            console.log(budgetDATA);
         }
     }
 
@@ -63,7 +120,12 @@ let UIcontroller = (function () {
         inputType: '.input-type',
         inputDesc: '.add-desc',
         inputValue: '.add-value',
-        inputBtn: '.input-btn'
+        inputBtn: '.input-btn',
+        incList: '.main-income',
+        expList: '.main-expenses',
+        balance: '.balance-total',
+        incTotal: '.income-total',
+        expTotal: '.expenses-total'
     };
 
 
@@ -92,7 +154,6 @@ let UIcontroller = (function () {
 
             if (ID === 'header-menu-logo') {
                 fadeIn(logoInfo, infoData[key]);
-                console.log('true')
             } else {
                 fadeIn(iconInfo, infoData[key]);
             }
@@ -104,7 +165,6 @@ let UIcontroller = (function () {
 
             if (ID === 'header-menu-logo') {
                 fadeOut(logoInfo, infoData[key]);
-                console.log('false')
             } else {
                 fadeOut(iconInfo, infoData[key]);
             }
@@ -118,6 +178,32 @@ let UIcontroller = (function () {
             }
         },
 
+        addItemUI: function (type, object) {
+            let html, newHtml, selector;
+
+            html = '<div id="' + type + '-%id"><span>%desc - </span><span>%val</span></div>';
+
+            if (type === 'inc') {
+                selector = document.querySelector(stringsDOM.incList);
+            } else if (type === 'exp') {
+                selector = document.querySelector(stringsDOM.expList);
+            }
+
+            newHtml = html.replace('%id', object.id);
+            newHtml = newHtml.replace('%desc', object.desc);
+            newHtml = newHtml.replace('%val', object.value);
+
+            selector.insertAdjacentHTML('beforeend', newHtml);
+
+        },
+
+        updateUI: function (objData) {
+            document.querySelector(stringsDOM.balance).textContent = objData.balance < 0 ? objData.balance : (objData.balance == 0 ?
+                objData.balance : '+' + objData.balance);
+            document.querySelector(stringsDOM.incTotal).textContent = objData.inc == 0 ? '- - -' : '+' + objData.inc;
+            document.querySelector(stringsDOM.expTotal).textContent = objData.exp == 0 ? '- - -' : '-' + objData.exp;
+        },
+
         stringsDOM: function () {
             return stringsDOM;
         }
@@ -129,32 +215,85 @@ let UIcontroller = (function () {
 
 let appController = (function (ctrData, ctrUI) {
 
-    let DOM = ctrUI.stringsDOM();
+    let addItem = function () {
 
-    let navBar = document.querySelector('.header-menu-container');
+        //get input
+        let input = ctrUI.getInput();
+        //add item to data structure
+        let newItem = ctrData.addItem(input.type, input);
+        //add item to ui
+        ctrUI.addItemUI(input.type, newItem);
+        //update budget
+        updateBudget();
 
-    let addBtn = document.querySelector(DOM.inputBtn);
+    }
 
-    let findKey = (object, value) => Object.keys(object).find(key => object[key] === value ? key : null)
+    let updateBudget = function () {
+        ctrData.calcBudget();
+        let budgetInfo = ctrData.getData();
+        ctrUI.updateUI(budgetInfo);
+    }
 
-    navBar.addEventListener('mouseover', (event) => {
-        let key = findKey(DOM, event.target.id);
-        if (key) {
-            ctrUI.showInfo(key, event.target.id);
-        };
-    });
+    let findKey = (object, value) => Object.keys(object).find(key => object[key] === value ? key : null);
 
-    navBar.addEventListener('mouseout', (event) => {
-        let key = findKey(DOM, event.target.id);
-        if (key) {
-            ctrUI.hideInfo(key, event.target.id);
-        };
-    });
+    let startMenu = function (key) {
+        menuFunctions[key]();
+    }
 
-    addBtn.addEventListener('click', () => {
-        console.log(ctrUI.getInput());
-    })
-    
+    let menuFunctions = {
+        newBudget: function () {
+            console.log('New budget!');
+        },
+
+        clearData: function () {
+            ctrData.clearBudget();
+            updateBudget();
+        }
+    }
+
+    let setupEvents = function () {
+
+
+        let DOM = ctrUI.stringsDOM();
+
+        let navBar = document.querySelector('.header-menu-container');
+
+        let addBtn = document.querySelector(DOM.inputBtn);
+
+
+
+        navBar.addEventListener('mouseover', (event) => {
+            let key = findKey(DOM, event.target.id);
+            if (key) {
+                ctrUI.showInfo(key, event.target.id);
+            };
+        });
+
+        navBar.addEventListener('mouseout', (event) => {
+            let key = findKey(DOM, event.target.id);
+            if (key) {
+                ctrUI.hideInfo(key, event.target.id);
+            };
+        });
+
+        navBar.addEventListener('click', (event) => {
+            let key = findKey(DOM, event.target.id);
+            if (key) {
+                startMenu(key);
+            };
+        })
+        addBtn.addEventListener('click', addItem);
+    }
+
+    return {
+        start: function () {
+            setupEvents();
+            menuFunctions.clearData();
+            console.log('Application started.')
+        }
+    }
 
 
 })(dataController, UIcontroller);
+
+appController.start();
